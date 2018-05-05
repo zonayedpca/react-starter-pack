a3b807d8b8e8175b69cd16bdf034577a4e3c0f64
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-05-05 17:27:22 +0530

    Typo

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="app">
       <img src={reactIMG} />
-      <h1>React Basic Starter Pack</h1>
+      <h1>React Starter Pack</h1>
       <p>Start Your Development from <code>src/index.js</code></p>
     </div>
   )
